7a9df428c15dc854f58dfb08a14b232946452c68
Harry Michael <ando.lalaina1103@gmail.com>
2021-07-14 16:11:40 +0300

    Added Nav Components/ Added Content Components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Navbar/>
+  <Content illustration-url="https://picsum.photos/500/300" contentLink="accueil"/>
+  <Content illustration-url="https://picsum.photos/500/300" contentLink="presentation"/>
+  <Content illustration-url="https://picsum.photos/200/300" contentLink="projets"/>
+  <Content illustration-url="https://picsum.photos/600/300" contentLink="contacts"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import Navbar from "./components/Navbar.vue"
+import Content from "./components/Content.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Navbar,
+    Content
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  :root {
+    --dark : #06050F;
+    --primary : #9B94DB;
+    --green : #188b83;
+    --red : #D05353;
+    --white : #FEFFFE;
+
+    font-size: 22px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  body {
+    background: linear-gradient(var(--dark), var(--primary));
+    height: 400vh;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+
+  body::-webkit-scrollbar {
+    display: none;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
 </style>
